b37e43bcd518b3bb623e4745a950e780b83ed83e
Samrat Neupane <71063668+SamratNeupane787@users.noreply.github.com>
2024-07-15 23:47:14 +0545

    key prop

--- src/app/components/Happening/Happening.jsx
+++ src/app/components/Happening/Happening.jsx
@@ -10,7 +10,10 @@ function Happening() {
       <div className=" grid  gap-4 place-items-center py-12 sm:grid-cols-1 md:grid-cols-3">
         {data?.map((item) => {
           return (
-            <div className=" w-full md:w-96 border-2 border-[#505052] rounded-lg px-8 py-8">
+            <div
+              key={item.eventName}
+              className=" w-full md:w-96 border-2 border-[#505052] rounded-lg px-8 py-8"
+            >
               <div className=" flex items-center justify-center flex-wrap">
                 <div>
                   <Image src="/chat.png" width={120} height={120} />
